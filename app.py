d4a2030293d2e32474ded282321233f076e47773
Aleksanteri <203408870+aSkelanturi@users.noreply.github.com>
2025-03-29 20:13:50 +0200

    Tried to add chug adding function halfway done, will continue later

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ app.secret_key = config.secret_key
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#Chug adding form
+@app.route("/new_chug")
+def new_chug():
+    return render_template("new_chug.html")
+
+#Adding chugs
+@app.route("/create_chug", methods=["POST"])
+def create_chug():
+
 # Registering page
 @app.route("/register")
 def register():
@@ -43,10 +53,12 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
-
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["pasword_hash"]
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -56,4 +68,5 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE chugs (
+    id INTEGER PRIMARY KEY,
+    drink TEXT,
+    clock TIME,
+    amount INTEGER,
+    alcohol INTEGER,
+    user_id INTEGER REFRENCES users
+);
